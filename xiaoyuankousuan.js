edaa9a0b63616d07fbeaa9ace8c0a4c4016cb119
CharlieDijkstra <102453792+CharlieDijkstra@users.noreply.github.com>
2024-10-13 18:03:57 +0800

    更新 xiaoyuankousuan.js

--- xiaoyuankousuan.js
+++ xiaoyuankousuan.js
@@ -4,7 +4,7 @@ const scriptName = "小猿口算pk";
 let responseBody = $response.body;
 
 // 使用正则表达式替换 URL
-responseBody = responseBody.replace(/https?:\/\/leo\.fbcontent\.cn\/bh5\/leo-web-oral-pk\/exercise[^\/]+\.js/g, 'https://cdn.rawgit.com/CharlieDijkstra/xiaoyuankousuan_ios/refs/heads/main/exercise_iphone.js');
+responseBody = responseBody.replace(/https?:\/\/leo\.fbcontent\.cn\/bh5\/leo-web-oral-pk\/exercise[^\/]+\.js/g, 'https://cdn.rawgit.com/CharlieDijkstra/xiaoyuankousuan_ios/refs/heads/main/exercise_new.js');
 
 // 返回修改后的响应体
 $response.body = responseBody;
