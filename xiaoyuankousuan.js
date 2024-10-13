55f5c3b8702aff2ab5696fcc41703cefba81b605
CharlieDijkstra <102453792+CharlieDijkstra@users.noreply.github.com>
2024-10-13 15:12:17 +0800

    Update xiaoyuankousuan.js

--- xiaoyuankousuan.js
+++ xiaoyuankousuan.js
@@ -1,18 +1,14 @@
 const scriptName = "小猿口算pk";
 console.log('this is a test log')
 // 检查请求 URL 是否符合匹配条件
-if (/^https?:\/\/xyks\.yuanfudao\.com\/bh5\/leo-web-oral-pk\/exercise.+/.test($request.url)) {
-    try {
-        // 解析响应体内容
-        let responseBody = $response.body;
 
-        // 使用正则表达式替换 URL
-        responseBody = responseBody.replace(/https?:\/\/leo\.fbcontent\.cn\/bh5\/leo-web-oral-pk\/exercise[^\/]+\.js/g, 'https://cdn.rawgit.com/CharlieDijkstra/xiaoyuankousuan_ios/refs/heads/main/exercise_iphone.js');
+// 解析响应体内容
+let responseBody = $response.body;
 
-        // 返回修改后的响应体
-        $response.body = responseBody;
-    } catch (err) {
-        // 捕获并记录错误
-        $logger.error(`小猿口算请求修改出现异常：${err}`);
-    }
+// 使用正则表达式替换 URL
+responseBody = responseBody.replace(/https?:\/\/leo\.fbcontent\.cn\/bh5\/leo-web-oral-pk\/exercise[^\/]+\.js/g, 'https://cdn.rawgit.com/CharlieDijkstra/xiaoyuankousuan_ios/refs/heads/main/exercise_iphone.js');
+
+// 返回修改后的响应体
+$response.body = responseBody;
+    
 }
